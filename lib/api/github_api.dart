6798279764b5ee71947e905532d048c82e48e876
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-24 22:50:40 -0300

    primeiro commit

---
+++ lib/api/github_api.dart
@@ -0,0 +1,42 @@
+// https://api.github.com/users/octocat
+// https://api.github.com/users/octocat/following
+
+// https://api.github.com/users/octocat
+// https://api.github.com/users/octocat/following
+
+//https://gist.github.com/dcvieira/c6761dae253759d2f788dc552d35cb8d
+
+import 'dart:convert';
+
+import '../models/user.dart';
+import 'package:http/http.dart' as http;
+
+class GithubApi {
+  final String baseUrl = 'https://api.github.com/';
+  final String token = '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';
+
+  Future<User?> findUser(String userName) async {
+    final url = '${baseUrl}users/$userName';
+    var response = await http.get(
+      Uri.parse(url),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      var json = jsonDecode(response.body);
+      var user = User.fromJson(json);
+
+      return user;
+    } else {
+      return null;
+    }
+  }
+
+  Future<List<User>> findList() async {
+    return [];
+  }
+}
